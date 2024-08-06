d52f30845dad691e7a232058a6da0c8eb7305de6
Wan <158989003+ridhwanmnr@users.noreply.github.com>
2024-08-06 18:57:51 +0800

    Added Database, Views and Controller

---
+++ Data/SchoolManagementDbContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace SchoolManagementApp.MVC.Data
+{
+    public partial class SchoolManagementDbContext : DbContext
+    {
+        public SchoolManagementDbContext()
+        {
+        }
+
+        public SchoolManagementDbContext(DbContextOptions<SchoolManagementDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Course> Courses { get; set; } = null!;
+        public virtual DbSet<Lecturer> Lecturers { get; set; } = null!;
+        public virtual DbSet<Student> Students { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>(entity =>
+            {
+                entity.HasIndex(e => e.Code, "UQ__Courses__A25C5AA705B2FB87")
+                    .IsUnique();
+
+                entity.Property(e => e.Code).HasMaxLength(5);
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Lecturer>(entity =>
+            {
+                entity.Property(e => e.FirstName).HasMaxLength(50);
+
+                entity.Property(e => e.LastName).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.DateOfBirth).HasColumnType("date");
+
+                entity.Property(e => e.FirstName).HasMaxLength(50);
+
+                entity.Property(e => e.LastName).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using SchoolManagementApp.MVC.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var conn = builder.Configuration.GetConnectionString("SchoolManagementDbConnection");
+builder.Services.AddDbContext<SchoolManagementDbContext>(options => options.UseSqlServer(conn));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
